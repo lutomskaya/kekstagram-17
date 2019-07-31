90a9e66f58514d2f18419cbaae27465b05f998fa
lutomskaya <lutomskaya@gmail.com>
2019-07-31 07:24:26 +0300

    Исправление

--- js/big-picture.js
+++ js/big-picture.js
@@ -5,7 +5,7 @@
   var bigPicture = document.querySelector('.big-picture');
   var urlPicture = bigPicture.querySelector('.big-picture__img img');
   var likesCount = bigPicture.querySelector('.likes-count');
-  var commentsCount = bigPicture.querySelector('.comments-count');
+  // var commentsCount = bigPicture.querySelector('.comments-count');
   var commentsList = bigPicture.querySelector('.social__comments');
   var commentsCountElement = bigPicture.querySelector('.social__comment-count');
   var commentLoader = bigPicture.querySelector('.comments-loader');
@@ -40,65 +40,45 @@
     return li;
   };
 
-  var generateTextCommentCount = function (count) {
-    commentsCountElement.textContent = count + ' из ' + commentsCount.textContent + ' комментариев';
-  };
+  var showBigPicture = function (photo) {
+    urlPicture.src = photo.url;
+    likesCount.textContent = photo.likes;
+    description.textContent = photo.description;
+    renderComments(photo.comments);
 
-  var generateBigPicture = function (element) {
-    urlPicture.src = element.url;
-    likesCount.textContent = element.likes;
-    commentsCount.textContent = element.comments.length;
-    description.textContent = element.description;
-  };
-
-  var insertComments = function (comments) {
-    var fragment = document.createDocumentFragment();
-    for (var i = 0; i < comments.length; i++) {
-      fragment.appendChild(renderComment(comments[i]));
-    }
-    commentsList.appendChild(fragment);
-  };
-
-  var showBigPicture = function (photos) {
-    var comments = photos.comments.slice();
-    var addComments = comments;
-    var nextComments = comments;
-    var nextAddComments = [];
-    var textCommentCount = addComments.length;
-    commentsList.innerHTML = '';
-    commentsCountElement.classList.remove('visually-hidden');
     bigPicture.classList.remove('hidden');
-    commentLoader.classList.remove('hidden');
     closeButton.addEventListener('click', closeWindow);
     document.addEventListener('keydown', onPopupEscPress);
-    generateBigPicture(photos);
 
-    var onCommentsLoaderClick = function () {
-      if (nextComments.length < COMMENT_COUNT) {
-        nextAddComments = nextComments;
-        commentLoader.classList.add('hidden');
-        commentLoader.removeEventListener('click', onCommentsLoaderClick);
-      } else {
-        nextAddComments = nextComments.slice(0, COMMENT_COUNT);
-        nextComments = nextComments.slice(nextAddComments.length);
+    function renderComments(comments) {
+      var currentIndex = 0;
+
+      commentsList.innerHTML = '';
+      commentsCountElement.classList.remove('visually-hidden');
+      commentLoader.classList.remove('hidden');
+
+      renderCommentsStep();
+      commentLoader.addEventListener('click', onLoaderClick);
+
+      function renderCommentsStep() {
+        for (var i = currentIndex; i < Math.min(currentIndex + COMMENT_COUNT, comments.length); i++) {
+          commentsList.appendChild(renderComment(comments[i]));
+        }
+
+        currentIndex = i;
+        commentsCountElement.textContent = currentIndex + ' из ' + comments.length + ' комментариев';
+
+        if (currentIndex === comments.length) {
+          commentLoader.classList.add('hidden');
+          commentLoader.removeEventListener('click', onLoaderClick);
+        }
       }
 
-      textCommentCount += nextAddComments.length;
-      generateTextCommentCount(textCommentCount);
-
-      insertComments(nextAddComments);
-    };
-
-    if (comments.length > COMMENT_COUNT) {
-      addComments = comments.slice(0, COMMENT_COUNT);
-      commentLoader.classList.remove('hidden');
-      textCommentCount = addComments.length;
-      nextComments = comments.slice(addComments.length);
-      commentLoader.addEventListener('click', onCommentsLoaderClick);
+      function onLoaderClick(evt) {
+        evt.preventDefault();
+        renderCommentsStep();
+      }
     }
-
-    insertComments(addComments);
-    generateTextCommentCount(textCommentCount);
   };
 
   window.bigPicture = {
